afc599db1c5481e2ae998504e0316e79ab734d9c
imadeamistaek <imadeamistaek@gmail.com>
2026-05-22 16:18:38 +0100

    Update meta tags and footer content for improved clarity and accuracy

--- src/lib/sections/homepage/footer.svelte
+++ src/lib/sections/homepage/footer.svelte
@@ -6,8 +6,8 @@
 <footer>
 	<Box as="div" customClass="col-6 col-start-1 -space-l -overflow-h" boxed>
 		<Grid customClass="col-6 col-start-1 -padding-xs">
-            <p class="body_text -micro col-3 col-start-1">Design systems for sustainability tech companies.</p>
-			<p class="body_text -micro col-3 col-start-4 -tright">© 2025 Mistaek. All rights reserved.</p>
+            <p class="body_text -micro col-3 col-start-1">Design systems for Series A sustainability tech companies. Make your science matter.</p>
+			<p class="body_text -micro col-3 col-start-4 -tright">© 2026 Mistaek. All rights reserved.</p>
 		</Grid>
 	</Box>
 </footer>
